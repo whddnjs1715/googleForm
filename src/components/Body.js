0261e747ed668483c0eb96a2616d94bd22e19b8d
whddnjs1715 <whddnjs1715@gmail.com>
2022-07-28 23:17:42 +0900

    [feat] Question_form 추가_2

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,4 +1,7 @@
+import { useState } from 'react';
 import styled from 'styled-components';
+
+const Container = styled.div``;
 
 const Question_form = styled.div`
   background-color: #f4f4f9;
@@ -11,31 +14,94 @@ const Section = styled.div`
   width: 50%;
 `;
 
+const Question_title_section = styled.div``;
+
+const Question_form_top = styled.div`
+  background-color: white;
+  border-top: 8px solid rgb(103, 58, 183);
+  border-radius: 8px;
+  padding: 30px 25px;
+  text-transform: capitalize;
+`;
+
+const Question_form_top_name = styled.input`
+  box-sizing: border-box;
+  font-family: 'Google sans', Roboto, Arial, sans-serif;
+  font-size: 32px;
+  font-weight: 400;
+  line-height: 40px;
+  line-height: 135%;
+  width: 100%;
+  border: none;
+  outline: none;
+  border-bottom: 1px solid #f4f4f9;
+  color: black;
+  height: 35px;
+`;
+
+const Question_form_top_desc = styled.input`
+  box-sizing: border-box;
+  margin-top: 10px;
+  font-family: 'Google sans', Roboto, Arial, sans-serif;
+  font-size: 13px;
+  font-weight: 400;
+  line-height: 40px;
+  width: 100%;
+  border: none;
+  outline: none;
+  border-bottom: 1px solid #f4f4f9;
+  color: black;
+  height: 20px;
+`;
+
 const Body = () => {
+  const [questions, setQuestions] = useState([
+    {
+      questionText: 'Which is the capital ?',
+      questionType: 'radio',
+      options: [
+        { optionText: 'Beng1' },
+        { optionText: 'Beng2' },
+        { optionText: 'Beng3' },
+        { optionText: 'Beng4' },
+      ],
+      open: true,
+      required: false,
+    },
+  ]);
+
+  const questionsUI = () => {
+    return questions.map((ques, i) => {
+      <div>
+      </div>;
+    });
+  };
+
   return (
-    <div>
+    <Container>
       <Question_form className="question_form">
-        <br></br>
+        <br />
         <Section className="section">
-          <div className="question_title_section">
-            <div className="question_form_top">
-              <input
+          <Question_title_section className="question_title_section">
+            <Question_form_top className="question_form_top">
+              <Question_form_top_name
                 type="text"
                 className="question_form_top_name"
                 style={{ color: 'black' }}
                 placeholder="Untitled document"
-              ></input>
-              <input
+              ></Question_form_top_name>
+              <Question_form_top_desc
                 type="text"
                 className="question_form_top_desc"
                 style={{ color: 'black' }}
                 placeholder="Form description"
-              ></input>
-            </div>
-          </div>
+              ></Question_form_top_desc>
+            </Question_form_top>
+          </Question_title_section>
+          {questionsUI()}
         </Section>
       </Question_form>
-    </div>
+    </Container>
   );
 };
 
